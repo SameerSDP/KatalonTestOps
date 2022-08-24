d0702688e0270af325887a7790703e7cd9d7d2a7
SameerSDP <sameer.dhananjay@indiumsoft.com>
2022-08-24 12:27:16 +0530

    .

---
+++ Test Suites/Sort_Filter.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sort_Filter</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>11ad3537-6b2b-4dd4-a8af-7a67b6b484d1</testSuiteGuid>
+   <testCaseLink>
+      <guid>ed545248-59c1-4501-8d7b-5ba490922f4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test/TC_03_Shop_Price_Filter</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
